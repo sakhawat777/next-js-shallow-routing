a6366b944f3ee8629122aa8d63518eaa1b9fb010
sakhawat777 <biplobbms@gmail.com>
2022-04-16 13:57:54 +0600

    Shallow Routing Done

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { format } from 'url';
 
-const index = () => {
+let counter = 0;
+
+export async function getServerSideProps() {
+	counter++;
+	return { props: { initialPropsCounter: counter } };
+}
+
+export default function Index({ initialPropsCounter }) {
+	const router = useRouter();
+	const { pathname, query } = router;
+	const reload = () => {
+		router.push(format({ pathname, query }));
+	};
+	const incrementCounter = () => {
+		const currentCounter = query.counter ? parseInt(query.counter) : 0;
+		const href = `/?counter=${currentCounter + 1}`;
+
+		router.push(href, href, { shallow: true });
+	};
+
 	return (
 		<div>
-			<h1>This is home page</h1>
+			<h2>This is the Home Page</h2>
+			<Link href='/about'>
+				<a>About</a>
+			</Link>
+			<button onClick={reload}>Reload</button>
+			<button onClick={incrementCounter}>Change State Counter</button>
+			<p>"getServerSideProps" ran for "{initialPropsCounter}" times.</p>
+			<p>Counter: "{query.counter || 0}".</p>
 		</div>
 	);
-};
-
-export default index;
+}
